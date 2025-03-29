0c1d87cbd468d65c696ad352e823467872b8baf9
FGaragnani <filippogaragnani1@gmail.com>
2025-03-29 16:41:54 +0100

    Update flowchart_transducer.py

--- src/transducer/mermaid/flowchart_transducer.py
+++ src/transducer/mermaid/flowchart_transducer.py
@@ -2,6 +2,7 @@ from typing import List
 
 from src.wellknown_diagram import WellKnownDiagram
 from core.representation.representation import DiagramRepresentation
+from src.representation.flowchart_representation.flowchart_representation import FlowchartRepresentation
 from src.unified_representation import UnifiedDiagramRepresentation
 from core.transducer.outcome import Outcome
 from core.transducer.transducer import Transducer
@@ -19,6 +20,12 @@ class FlowchartToMermaidTransducer(Transducer):
         ]
 
     def elaborate(self, diagram_id: str, diagram_representation: DiagramRepresentation) -> Outcome:
-        assert isinstance(diagram_representation, UnifiedDiagramRepresentation)
-        # TODO: il codice per ottenere il mermaid del flow chart (e graph) qui
+        assert isinstance(diagram_representation, FlowchartRepresentation)
+        
+        body: str = ""
+        
+        #TODO: Stuff to do with the diagram representation
+        
+        outcome: Outcome = Outcome(diagram_id, body)
+        return outcome
 
