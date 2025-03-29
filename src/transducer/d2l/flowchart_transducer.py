2d999f15c1ae4dc845069b351892919b7d47c223
FGaragnani <filippogaragnani1@gmail.com>
2025-03-29 16:13:07 +0100

    D2L Transducer interface

---
+++ src/transducer/d2l/flowchart_transducer.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from src.wellknown_diagram import WellKnownDiagram
+from core.representation.representation import DiagramRepresentation
+from src.unified_representation import UnifiedDiagramRepresentation
+from core.transducer.outcome import Outcome
+from core.transducer.transducer import Transducer
+
+
+class FlowchartToD2LTransducer(Transducer):
+
+    def __init__(self, identifier: str):
+        super().__init__(identifier)
+
+    def compatible_diagrams(self) -> List[str]:
+        return [
+            WellKnownDiagram.FLOW_CHART.value,
+            WellKnownDiagram.GRAPH_DIAGRAM.value,
+        ]
+
+    def elaborate(self, diagram_id: str, diagram_representation: DiagramRepresentation) -> Outcome:
+        assert isinstance(diagram_representation, UnifiedDiagramRepresentation)
+        # TODO: il codice per ottenere il mermaid del flow chart (e graph) qui
