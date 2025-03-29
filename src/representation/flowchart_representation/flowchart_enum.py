a2fca09b0738c4d7b17ccea92eb6a4a585a03439
FGaragnani <filippogaragnani1@gmail.com>
2025-03-29 19:21:16 +0100

    Implemented flowchart Mermaid transducer

--- src/representation/flowchart_representation/flowchart_enum.py
+++ src/representation/flowchart_representation/flowchart_enum.py
@@ -4,20 +4,15 @@ from enum import Enum
 For reference: https://en.wikipedia.org/wiki/Flowchart#Building_blocks
 """
 
-"""
-    NOTA:
-           Da parlarne insieme magari. Ci sono le annotazioni: il collegamento (la linea) se la includiamo
-            deve essere una relazione (AnnotationLink) mentre il testo deve essere un'entità ("AnnotationNode").
-            Giusto?
-"""
 class FlowchartElementEnum(Enum):
     TERMINAL: str = "TerminalNode"
     PROCESS: str = "ProcessNode"
     DECISION: str = "DecisionNode"
     INPUT_OUTPUT: str = "InputOutputNode"
-    ANNOTATION: str = "AnnotationNode"
+    CIRCLE: str = "CircleNode"
     SUBROUTINE: str = "SubroutineNode"
     
 class FlowchartRelationEnum(Enum):
     ARROW: str = "Arrowhead"
-    ANNOTATION_LINK: str = "AnnotationLink"
+    OPEN_LINK: str = "OpenLink"
+    DOTTED_ARROW: str = "DottedArrowhead"
--- src/transducer/mermaid/flowchart_transducer.py
+++ src/transducer/mermaid/flowchart_transducer.py
@@ -7,6 +7,7 @@ from src.unified_representation import UnifiedDiagramRepresentation
 from core.transducer.outcome import Outcome
 from core.transducer.transducer import Transducer
 
+from src.representation.flowchart_representation.flowchart_enum import FlowchartElementEnum, FlowchartRelationEnum
 
 class FlowchartToMermaidTransducer(Transducer):
 
@@ -19,12 +20,47 @@ class FlowchartToMermaidTransducer(Transducer):
             WellKnownDiagram.GRAPH_DIAGRAM.value,
         ]
 
+    @staticmethod
+    def wrap_element(category: FlowchartElementEnum, label: str) -> str:
+        match category:
+            case FlowchartElementEnum.CIRCLE:
+                return f"(({label}))"
+            case FlowchartElementEnum.TERMINAL:
+                return f"([{label}])"
+            case FlowchartElementEnum.PROCESS:
+                return f"({label})"
+            case FlowchartElementEnum.DECISION:
+                return "{" + label + "}"
+            case FlowchartElementEnum.INPUT_OUTPUT:
+                return f"[/{label}/]"
+            case FlowchartElementEnum.SUBROUTINE:
+                return f"[{label}]"
+            case _:
+                raise ValueError(f"Unknown flowchart element category: {category}")
+
+    @staticmethod
+    def wrap_relation(category: FlowchartRelationEnum, label: str) -> str:
+        match category:
+            case FlowchartRelationEnum.ARROW:
+                return f"-->|{label}|"
+            case FlowchartRelationEnum.OPEN_LINK:
+                return f"---|{label}|"
+            case FlowchartRelationEnum.DOTTED_ARROW:
+                return f" -. {label} .->"
+            case _:
+                raise ValueError(f"Unknown flowchart relation category: {category}")
+
     def elaborate(self, diagram_id: str, diagram_representation: DiagramRepresentation) -> Outcome:
         assert isinstance(diagram_representation, FlowchartRepresentation)
         
-        body: str = ""
-        
-        #TODO: Stuff to do with the diagram representation
+        body: str = "Flowchart TD\n"
+        for id, element in diagram_representation.elements.items():
+            body += f"\t{id.id}{self.wrap_element(element.category, element.label)}\n"
+            
+        body += "\n"
+        for relation in diagram_representation.relations:
+            body += f"\t{relation.source_id.id}"
+            body += f"{self.wrap_relation(relation.category, relation.label)}{relation.target_id.id}\n"
         
         outcome: Outcome = Outcome(diagram_id, body)
         return outcome
