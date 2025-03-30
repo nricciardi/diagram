3ba26044d65b56ba3b705c4425639e908fa4c991
FGaragnani <filippogaragnani1@gmail.com>
2025-03-30 15:44:18 +0200

    dump and load for flowchart rep

--- src/representation/flowchart_representation/flowchart_representation.py
+++ src/representation/flowchart_representation/flowchart_representation.py
@@ -14,8 +14,47 @@ class FlowchartRepresentation(DiagramRepresentation):
     elements: Dict[str, Element]
     relations: List[Relation]
 
+    """
+        The syntax is: first all elements - with id, category and label separated by ;; -
+        then a new line and all relations - with category, source_id, target_id and label separeted by ;;
+    """
     def dump(self, output_path: str):
-        pass
+        
+        for id, element in self.elements.items():
+            with open(output_path, "a") as file:
+                file.write(f"{id};;{element.category};;{"" if element.label == None else element.label}\n")
+                
+        with open(output_path, "a") as file:
+            file.write("\n")
+            
+        for relation in self.relations:
+            with open(output_path, "a") as file:
+                file.write(f"{relation.category};;{"" if relation.source_id == None else relation.source_id};; \
+                           {"" if relation.target_id == None else relation.target_id};;{"" if relation.label == None else relation.label}\n")
+        
     
     def load(self, input_path: str):
-        pass
+        
+        with open(input_path, "r") as file:
+            lines: list[str] = file.readlines()
+        
+        self.elements = {}
+        self.relations = []
+        break_line = len(lines)
+        
+        for idx, line in enumerate(lines):
+            if line == "\n":
+                break_line = idx
+                break
+            id, category, label = line.split(";;")
+            label = (None if label == "" else label)
+            self.elements[id] = Element(id, category, label)
+            
+        lines = lines[break_line + 1:]
+        for line in lines:
+            category, source_id, target_id, label = line.split(";;")
+            source_id = (None if source_id == "" else source_id)
+            target_id = (None if target_id == "" else target_id)
+            label = (None if label == "" else label)
+            self.relations.append(Relation(category, source_id, target_id, label))
+
