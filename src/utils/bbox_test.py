9c772132324ba3349947630bb0a0dbb05baa5342
Saverio Napolitano <saverio.napolitano.2002@gmail.com>
2025-04-16 15:45:36 +0200

    Update bbox_utils.py

--- src/utils/bbox_test.py
+++ src/utils/bbox_test.py
@@ -1,7 +1,9 @@
 import unittest
+from typing import List, Tuple
+
 import torch
 from core.image.bbox.bbox2p import ImageBoundingBox2Points
-from src.utils.bbox_utils import bbox_distance, bbox_overlap
+from src.utils.bbox_utils import bbox_distance, bbox_overlap, bbox_split
 
 
 class TestUtils(unittest.TestCase):
@@ -18,6 +20,19 @@ class TestUtils(unittest.TestCase):
         box2: torch.Tensor = torch.tensor([3, 9, 5, 5])
         bbox2: ImageBoundingBox2Points = ImageBoundingBox2Points(category='text', box=box2, trust=1.0)
         self.assertEqual(bbox_overlap(bbox1=bbox1, bbox2=bbox2), 0.5)
+
+    def test_bbox_split(self):
+        expected_source: List[Tuple[float, float]] = [(5, 10), (8, 10), (8, 8.2), (5, 8.2)]
+        expected_middle: List[Tuple[float, float]] = [(5, 8.2), (8, 8.2), (8, 2.8), (5, 2.8)]
+        expected_target: List[Tuple[float, float]] = [(5, 2.8), (8, 2.8), (8, 1), (5, 1)]
+
+        box1: torch.Tensor = torch.tensor([5, 10, 8, 1], dtype=torch.float64)
+        bbox1: ImageBoundingBox2Points = ImageBoundingBox2Points(category='node', box=box1, trust=1.0)
+        splits = bbox_split(bbox=bbox1, direction='height', ratios=[0.2, 0.6, 0.2], arrow_head='down')
+        self.assertEqual(expected_target, splits[2])
+        self.assertEqual(expected_middle, splits[1])
+        self.assertEqual(expected_source, splits[0])
+
 
 
 if __name__ == '__main__':
--- src/utils/bbox_utils.py
+++ src/utils/bbox_utils.py
@@ -1,5 +1,6 @@
 import math
 from typing import List, Tuple
+import numpy as np
 
 from core.image.bbox.bbox import ImageBoundingBox
 
@@ -40,21 +41,91 @@ def bbox_overlap(bbox1: ImageBoundingBox, bbox2: ImageBoundingBox) -> float:
 
 
 def bbox_vertices(bbox1: ImageBoundingBox, bbox2: ImageBoundingBox) -> Tuple[
-    List[Tuple[float, float]], List[Tuple[float, float]]]:
+        List[Tuple[float, float]], List[Tuple[float, float]]]:
     """
     Returns the vertices (as a list of coordinates (x, y) for each point) of two bboxes
     """
     bbox1_vertices: List[Tuple[float, float]] = [(bbox1.top_left_x, bbox1.top_left_y),
-                                                 (bbox1.bottom_left_x, bbox1.bottom_left_y),
                                                  (bbox1.top_right_x, bbox1.top_right_y),
-                                                 (bbox1.bottom_right_x, bbox1.bottom_right_y)]
+                                                 (bbox1.bottom_right_x, bbox1.bottom_right_y),
+                                                 (bbox1.bottom_left_x, bbox1.bottom_left_y)]
     bbox2_vertices: List[Tuple[float, float]] = [(bbox2.top_left_x, bbox2.top_left_y),
-                                                 (bbox2.bottom_left_x, bbox2.bottom_left_y),
                                                  (bbox2.top_right_x, bbox2.top_right_y),
-                                                 (bbox2.bottom_right_x, bbox2.bottom_right_y)]
+                                                 (bbox2.bottom_right_x, bbox2.bottom_right_y),
+                                                 (bbox2.bottom_left_x, bbox2.bottom_left_y)]
 
     return bbox1_vertices, bbox2_vertices
 
 
-def bbox_split(bbox: ImageBoundingBox, direction: str, ratios: List[int]):
-    pass
+def interpolate(p1: Tuple[float, float], p2: Tuple[float, float], t: float) -> np.ndarray:
+    return (1 - t) * np.array(p1) + t * np.array(p2)
+
+
+def bbox_split(bbox: ImageBoundingBox, direction: str, ratios: List[float], arrow_head: str) -> List[List[Tuple]]:
+    """
+    Splits the arrow bbox into three parts along the given direction and according to the given ratios
+    """
+
+    vertices, other_vertices = bbox_vertices(bbox1=bbox, bbox2=bbox)
+
+    if direction == 'height':
+        if arrow_head == 'down':
+            left = [vertices[0], vertices[3]]
+            right = [vertices[1], vertices[2]]
+        else:
+            left = [vertices[3], vertices[0]]
+            right = [vertices[2], vertices[1]]
+
+        boxes = []
+        t_start = 0
+
+        for ratio in ratios:
+            t_end = t_start + ratio
+
+            # Interpolate along the left and right edges
+            left_top = interpolate(left[0], left[1], t_start).tolist()
+            left_bottom = interpolate(left[0], left[1], t_end).tolist()
+            right_top = interpolate(right[0], right[1], t_start).tolist()
+            right_bottom = interpolate(right[0], right[1], t_end).tolist()
+
+            # Create sub-box (clockwise)
+            sub_box = [
+                tuple(left_top),
+                tuple(right_top),
+                tuple(right_bottom),
+                tuple(left_bottom)
+            ]
+            boxes.append(sub_box)
+
+            t_start = t_end
+    else:
+        if arrow_head == 'right':
+            top_edge = [vertices[0], vertices[1]]
+            bottom_edge = [vertices[3], vertices[2]]
+        else:
+            top_edge = [vertices[1], vertices[0]]
+            bottom_edge = [vertices[2], vertices[3]]
+
+        boxes = []
+        t_start = 0
+
+        for ratio in ratios:
+            t_end = t_start + ratio
+
+            # Interpolate on top and bottom edges
+            top_left = interpolate(top_edge[0], top_edge[1], t_start)
+            top_right = interpolate(top_edge[0], top_edge[1], t_end)
+            bottom_left = interpolate(bottom_edge[0], bottom_edge[1], t_start)
+            bottom_right = interpolate(bottom_edge[0], bottom_edge[1], t_end)
+
+            sub_box = [
+                tuple(top_left),
+                tuple(top_right),
+                tuple(bottom_right),
+                tuple(bottom_left)
+            ]
+            boxes.append(sub_box)
+
+            t_start = t_end
+
+    return boxes
