ea82062891fe5ec5ed4e0b83148166d23fbcda1e
FGaragnani <filippogaragnani1@gmail.com>
2025-05-26 23:36:08 +0200

    Update tensor_image.py
    Now loads .bmp files

--- core/image/tensor_image.py
+++ core/image/tensor_image.py
@@ -1,8 +1,10 @@
 from torchvision.io import read_image
+import cv2
 
 from core.image.image import Image
 from dataclasses import dataclass
 from torch import Tensor
+import torch
 
 
 @dataclass
@@ -11,7 +13,13 @@ class TensorImage(Image):
 
     @staticmethod
     def from_str(path: str) -> 'Image':
-        return TensorImage(read_image(path))
+        if (path.lower().endswith('.png') or path.lower().endswith('.jpeg')):
+            return TensorImage(read_image(path))
+        if (path.lower().endswith('.bmp')):
+            np_img = cv2.imread(path)
+            tensor = torch.from_numpy(np_img).permute(2, 0, 1)
+            return TensorImage(tensor)
+            
 
     def as_tensor(self) -> Tensor:
         return self.tensor
