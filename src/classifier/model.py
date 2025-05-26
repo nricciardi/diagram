cec3ff8f74a9670f24cdc57fb9b731d56db46699
FGaragnani <filippogaragnani1@gmail.com>
2025-05-26 14:37:52 +0200

    Update model.py

--- src/classifier/model.py
+++ src/classifier/model.py
@@ -11,32 +11,50 @@ class ClassifierCNN(nn.Module):
     
     def __init__(self, num_classes: int):
         super(ClassifierCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 2, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(2, 4, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(4, 8, kernel_size=3, stride=1, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.fc1 = nn.Linear(64 * 8 * 8, 512)
-        self.fc2 = nn.Linear(512, 128)
-        self.fc3 = nn.Linear(128, num_classes)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=0.5)
+        
+        class ConvLayer(nn.Module):
+            """
+            A convolutional layer followed by ReLU activation and max pooling.
+            """
+            def __init__(self, in_channels: int, out_channels: int, kernel_size: int = 3, stride: int = 1, padding: int = 1, kernel_pool_size: int = 2, pool_stride: int = 2, pool_padding: int = 0):
+                super(ConvLayer, self).__init__()
+                self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+                self.relu = nn.ReLU()
+                self.pool = nn.MaxPool2d(kernel_size=kernel_pool_size, stride=pool_stride, padding=pool_padding)
+            def forward(self, x):
+                x = self.conv(x)
+                x = self.relu(x)
+                x = self.pool(x)
+                return x
+        
+        class LinearLayer(nn.Module):
+            """
+            A linear layer followed by ReLU activation.
+            """
+            def __init__(self, in_features: int, out_features: int, dropout_rate: float = 0.5):
+                super(LinearLayer, self).__init__()
+                self.linear = nn.Linear(in_features, out_features)
+                self.dropout = nn.Dropout(p=dropout_rate)
+                self.relu = nn.ReLU()
+            def forward(self, x):
+                x = self.linear(x)
+                x = self.relu(x)
+                x = self.dropout(x)
+                return x
+        
+        self.conv1 = ConvLayer(1, 2)
+        self.conv2 = ConvLayer(2, 4)
+        self.conv3 = ConvLayer(4, 8)
+        self.fc1 = LinearLayer(64*8*8, 512)
+        self.fc2 = LinearLayer(512, 128)
+        self.fc3 = LinearLayer(128, num_classes)
         self.sequential = nn.Sequential(
             self.conv1,
-            self.relu,
-            self.pool,
             self.conv2,
-            self.relu,
-            self.pool,
             self.conv3,
-            self.relu,
-            self.pool,
             nn.Flatten(),
             self.fc1,
-            self.relu,
-            self.dropout,
             self.fc2,
-            self.relu,
-            self.dropout,
             self.fc3,
             nn.Softmax(dim=1)
         )
