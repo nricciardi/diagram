aaf954bf74b5644443076dbeaa49a6fefebe9b8f
FGaragnani <filippogaragnani1@gmail.com>
2025-05-26 15:38:18 +0200

    Training classifier script

---
+++ src/classifier/training.py
@@ -0,0 +1,11 @@
+import os, sys
+sys.path.append(os.path.dirname("."))
+sys.path.append(os.path.dirname("/"))
+
+from src.classifier.classifier import GNRClassifier
+
+if __name__ == "__main__":
+    
+    classifier = GNRClassifier()
+    classifier.train()
+    classifier.save_model(path="src/classifier/classifier.pth")
