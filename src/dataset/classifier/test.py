d842d961e025c201ee198b2da4cfd6a86ea65299
Saverio Napolitano <saverio.napolitano.2002@gmail.com>
2025-05-26 15:49:00 +0200

    Fix classifier dataset folder

--- src/dataset/classifier/test.py
+++ src/dataset/classifier/test.py
@@ -17,12 +17,12 @@ class TestClassifierDataset(unittest.TestCase):
         self.assertEqual(self.dataset.__len__(), 9216)
 
     def test_get_item(self):
-        image, label = self.dataset.__getitem__(0)
+        image, label = self.dataset.__getitem__(1098)
         im = cv2.imread(
-            '/Users/saverionapolitano/PycharmProjects/diagram/dataset/classifier/all/writer013_fa_007.png', cv2.IMREAD_UNCHANGED)
+            '/Users/saverionapolitano/PycharmProjects/diagram/dataset/classifier/all/writer017_fc_008b.png', cv2.IMREAD_UNCHANGED)
         tensor_im = torch.from_numpy(im)
         self.assertTrue(torch.equal(tensor_im[torch.newaxis, :, :], image.as_tensor()))
-        self.assertEqual(label, 'graph')
+        self.assertEqual(label, 'flowchart')
 
 
 if __name__ == '__main__':
