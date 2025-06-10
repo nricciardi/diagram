df4a03343ddcd535b229858667c36c99293b6e11
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-06-10 15:02:48 +0200

    Updated the ClassifierModel

--- src/classifier/model.py
+++ src/classifier/model.py
@@ -16,14 +16,14 @@ class ClassifierCNN(nn.Module):
             """
             A convolutional layer followed by ReLU activation and max pooling.
             """
-            def __init__(self, in_channels: int, out_channels: int, kernel_size: int = 3, stride: int = 1, padding: int = 1, kernel_pool_size: int = 3, pool_stride: int = 3, pool_padding: int = 0):
+            def __init__(self, in_channels: int, out_channels: int, kernel_size: int = 2, stride: int = 1, padding: int = 1, kernel_pool_size: int = 3, pool_stride: int = 3, pool_padding: int = 0):
                 super(ConvLayer, self).__init__()
                 self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
-                self.relu = nn.ReLU()
+                self.norm = nn.BatchNorm2d(out_channels)
                 self.pool = nn.MaxPool2d(kernel_size=kernel_pool_size, stride=pool_stride, padding=pool_padding)
             def forward(self, x):
                 x = self.conv(x)
-                x = self.relu(x)
+                x = self.norm(x)
                 x = self.pool(x)
                 return x
         
@@ -31,7 +31,7 @@ class ClassifierCNN(nn.Module):
             """
             A linear layer followed by ReLU activation.
             """
-            def __init__(self, in_features: int, out_features: int, dropout_rate: float = 0.5):
+            def __init__(self, in_features: int, out_features: int, dropout_rate: float = 0.2):
                 super(LinearLayer, self).__init__()
                 self.linear = nn.Linear(in_features, out_features)
                 self.dropout = nn.Dropout(p=dropout_rate)
@@ -42,14 +42,16 @@ class ClassifierCNN(nn.Module):
                 x = self.dropout(x)
                 return x
         
-        self.conv1 = ConvLayer(1, 2)
-        self.conv2 = ConvLayer(2, 4)
-        self.fc1 = LinearLayer(729 * 64, 512)
-        self.fc2 = LinearLayer(512, 128)
+        self.conv1 = ConvLayer(1, 8)
+        self.conv2 = ConvLayer(8, 16)
+        self.conv3 = ConvLayer(16, 24, kernel_size=3, kernel_pool_size=2)
+        self.fc1 = LinearLayer(8664, 2048)
+        self.fc2 = LinearLayer(2048, 128)
         self.fc3 = LinearLayer(128, num_classes)
         self.convSeq = nn.Sequential(
             self.conv1,
-            self.conv2
+            self.conv2,
+            self.conv3
         )
         self.linSeq = nn.Sequential(
             self.fc1,
