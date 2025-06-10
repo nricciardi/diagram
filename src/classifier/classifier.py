144a6a45d86977307e4ad698ca7babf27a17894e
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-06-10 15:02:06 +0200

    Method evaluate

--- src/classifier/classifier.py
+++ src/classifier/classifier.py
@@ -9,6 +9,7 @@ from torch.utils.data import random_split
 import torch.optim as optim
 import torch.nn as nn
 import torch
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 class GNRClassifier(Classifier):
     
@@ -16,7 +17,7 @@ class GNRClassifier(Classifier):
     A classifier that employs a CNN for image classification.
     
     :param model_path: Path to the pre-trained model. If not provided, a new model will be initialized.
-    :param classes: A dictionary mapping class indices to class names. Defaults to a predefined set of classes. IT'S HIGHLY RECCOMMENDED TO PASS IT!
+    :param classes: A dictionary mapping class indices to class names. Defaults to a predefined set of classes. IT'S HIGHLY RECCOMMENDED TO PASS IT AS DATASET.classes!
     :param processor: The image processor to use for preprocessing images. Defaults to GNRMultiProcessor.
     """
     def __init__(self, classes: dict[int, str] = None, model_path: str = "", processor: Processor = GNRMultiProcessor()):
@@ -27,12 +28,14 @@ class GNRClassifier(Classifier):
                 1: "graph",
                 2: "other"
             }
+            print("No classes provided, using default classes! Are you sure? It may cause unexpected behaviour.")
         self.classes = classes
         self.model : ClassifierCNN = None
         if model_path == "":
             self.model = ClassifierCNN(num_classes=len(classes.keys()))
         else:
-            self.model = ClassifierCNN(num_classes=len(classes.keys())).load(path=model_path)
+            self.model = ClassifierCNN(num_classes=len(classes.keys()))
+            self.model.load(model_path)
         self.processor = processor
     
     
@@ -51,27 +54,30 @@ class GNRClassifier(Classifier):
         return self.classes[predicted.item()]
     
     
-    def train(self, dataset: DatasetClassifier = DatasetClassifier(), epochs: int = 10, batch_size: int = 32, learning_rate: float = 0.001, verbose: bool = True):
+    def train(self, dataset: DatasetClassifier, epochs: int = 10, batch_size: int = 32, learning_rate: float = 1e-4, verbose: bool = True):
         
         """
         Train the model using the provided dataset.
         """
+
+        label_to_index = self.classes
+        assert self.classes == dataset.classes 
         
         ### Prepare the dataset
         dataset_size = len(dataset)
         val_size = int(0.2 * dataset_size)
         train_size = dataset_size - val_size
 
+        ### Split the dataset into training and validation sets
         train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
         train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
         val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
-        label_to_index = {v: k for k, v in self.classes.items()}
         
         ### Prepare the optimizer and loss function
-        optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
-        criterion = nn.CrossEntropyLoss()
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        optimizer = optim.AdamW(self.model.parameters(), lr=learning_rate)
+        criterion = nn.CrossEntropyLoss(weight=torch.tensor(dataset.weights, dtype=torch.float32).to(device))
         self.model.to(device)
         self.model.train()
         
@@ -79,9 +85,12 @@ class GNRClassifier(Classifier):
         for epoch in range(epochs):
             running_loss = 0.0
             for i, (images, labels) in enumerate(train_dataloader):
+                if (i + 1) % 20 == 0 and verbose:
+                    print(f"Epoch [{epoch + 1}/{epochs}], Step [{i + 1}/{len(train_dataloader)}]")
+
                 optimizer.zero_grad()
                 
-                outputs = self.model(images.float()).to(device)
+                outputs = self.model(images.float().to(device))
                 labels = torch.tensor([label_to_index[label] for label in labels], dtype=torch.long).to(device)
                 
                 loss = criterion(outputs, labels)
@@ -91,7 +100,7 @@ class GNRClassifier(Classifier):
                 running_loss += loss.item()
                 
             if verbose:
-                print(f"Epoch [{epoch + 1}/{epochs}], Step [{i + 1}/{len(train_dataloader)}], Loss: {running_loss / 10:.4f}")
+                print(f"Epoch [{epoch + 1}/{epochs}], Step [{i + 1}/{len(train_dataloader)}], Loss: {running_loss / len(train_dataloader) :.4f}")
                 
         ### Testing loop
         self.model.eval()
@@ -99,11 +108,13 @@ class GNRClassifier(Classifier):
         total = 0
         with torch.no_grad():
             for images, labels in val_dataloader:
-                outputs = self.model(images)
+                outputs = self.model(images.float().to(device))
+                labels = torch.tensor([label_to_index[label] for label in labels], dtype=torch.long).to(device)
+
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
-            
+        print(f"Validation Accuracy: {100 * correct / total:.2f}%")
     
     
     def save_model(self, path: str = ""):
@@ -132,3 +143,55 @@ class GNRClassifier(Classifier):
         if path == "":
             path = self.model_path
         self.model.load(path)
+
+    
+    def evaluate(self, dataset: DatasetClassifier = DatasetClassifier(), visual_output: bool = False, verbose: bool = False) -> float:
+        """
+        Evaluate the model on the provided dataset.
+        
+        :param dataset: The dataset to evaluate the model on.
+        :param visual_output: If True, will visualize some predictions.
+        :param verbose: If True, will print detailed evaluation information.
+        :return: The accuracy of the model on the dataset.
+        """
+
+        subset_size = int(0.1 * len(dataset))
+        _, subset = random_split(dataset, [len(dataset) - subset_size, subset_size])
+        dataloader = DataLoader(subset, batch_size=32, shuffle=True)
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(device)
+
+        self.model.eval()
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            all_preds = []
+            all_labels = []
+            for images, labels in dataloader:
+
+                labels = torch.tensor([self.classes[label] for label in labels], dtype=torch.long).to(device)
+                images = images.float().to(device)
+                outputs = self.model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+                all_preds.extend(predicted.cpu().numpy())
+                all_labels.extend(labels.cpu().numpy())
+
+            self.last_confusion_preds = all_preds
+            self.last_confusion_labels = all_labels
+        
+        accuracy = 100 * correct / total
+        classes_names = {k: v for v, k in self.classes.items()}
+        print(f"Accuracy of the model on the dataset: {accuracy:.2f}%")
+        if visual_output:
+            import matplotlib.pyplot as plt
+
+            cm = confusion_matrix(all_labels, all_preds)
+            disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[classes_names[i] for i in range(len(self.classes))])
+            fig, ax = plt.subplots(figsize=(6, 6))
+            disp.plot(ax=ax, cmap=plt.cm.Blues, values_format='d')
+            plt.title("Confusion Matrix")
+            plt.show()
+        return accuracy
