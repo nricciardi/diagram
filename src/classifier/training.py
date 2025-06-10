7d59d65954f99a4214b0eeda7b8474ffca41a126
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-06-10 15:02:35 +0200

    Update training.py
    Made the evaluation script

--- src/classifier/training.py
+++ src/classifier/training.py
@@ -1,11 +1,21 @@
-import os, sys
+import os, sys, torch
 sys.path.append(os.path.dirname("."))
 sys.path.append(os.path.dirname("/"))
 
 from src.classifier.classifier import GNRClassifier
+from src.classifier.dataset import DatasetClassifier
 
 if __name__ == "__main__":
     
-    classifier = GNRClassifier()
-    classifier.train(verbose=True)
+    """
+    Training script for the GNRClassifier.
+
+    dataset = DatasetClassifier()
+    classifier = GNRClassifier(classes=dataset.classes)
+    classifier.train(dataset, verbose=True, batch_size=64, epochs=10)
     classifier.save_model(path="src/classifier/classifier.pth")
+    """
+
+    dataset = DatasetClassifier()
+    classifier = GNRClassifier(dataset.classes, model_path="src/classifier/classifier.pth")
+    classifier.evaluate(visual_output=True, verbose=True)
