4d222e5e20e971eba4c72e82cd746850a41a7802
Nicola Ricciardi <ricciardincl@gmail.com>
2025-06-20 00:31:37 +0200

    Troubleshooting

--- src/dataset/extractor/dataset.py
+++ src/dataset/extractor/dataset.py
@@ -20,16 +20,22 @@ class ObjectDetectionDataset(Dataset):
 
     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None, synthetic_bbox_size: int = 64):
         with open(annotations_file, 'r') as f:
-            self.labels = json.load(f)
+            self.info = json.load(f)
+            self.images = {}
+            for img in self.info["images"]:
+                self.images[img["id"]] = img
+
+            self.annotations = self.info["annotations"]
+
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
         self.synthetic_bbox_size = synthetic_bbox_size
 
     def __len__(self):
-        return len(self.labels['images'])
+        return len(self.images)
 
-    def generate_synthetic_bbox(self, image: torch.Tensor, center_x: int, center_y: int, image_id: int) -> List[int]:
+    def generate_synthetic_bbox(self, image: torch.Tensor, center_x: int, center_y: int) -> List[int]:
         """
         Given an image, produce a synthetic bbox centered on position
 
@@ -40,9 +46,7 @@ class ObjectDetectionDataset(Dataset):
         _, H, W = image.shape
 
         if center_x >= W or center_y >= H:
-            raise ValueError(f"center_x: {center_x}, W: {W}, center_y: {center_y}, H: {H}, image_id: {image_id}")
-        assert center_x < W
-        assert center_y < H
+            raise ValueError(f"center_x: {center_x}, W: {W}, center_y: {center_y}, H: {H}")
 
         half_bbox = self.synthetic_bbox_size // 2
 
@@ -73,62 +77,61 @@ class ObjectDetectionDataset(Dataset):
         else:
             raise TypeError("image_tensor must be a torch.Tensor")
 
-        if image_np.shape[2] == 1:
-            image_np = image_np.squeeze(-1)
-            cmap = 'gray'
-        else:
-            cmap = None
+        plt.imshow(image_np)
 
-        fig, ax = plt.subplots(figsize=(6, 6))
-        ax.imshow(image_np, cmap=cmap)
-        ax.scatter(keypoints[:, 0], keypoints[:, 1], c='r', s=20, marker='x', label="Keypoints")
+        head_x = keypoints[3]
+        head_y = keypoints[4]
+
+        tail_x = keypoints[0]
+        tail_y = keypoints[1]
+
+        plt.scatter([tail_x], [tail_y], c='red', s=20, label="Keypoints")
+        plt.scatter([head_x], [head_y], c='blue', s=20, label="Keypoints")
 
         # Draw bounding box if provided
-        if bbox is not None and len(bbox) == 4:
-            x, y, w, h = bbox
-            rect = patches.Rectangle((x, y), w, h,
-                                     linewidth=2, edgecolor='lime', facecolor='none', label="BBox")
-            ax.add_patch(rect)
+        # if bbox is not None and len(bbox) == 4:
+        #     x, y, w, h = bbox
+        #     rect = patches.Rectangle((x, y), w, h,
+        #                              linewidth=2, edgecolor='lime', facecolor='none', label="BBox")
 
-        ax.set_title("Image with Keypoints and Bounding Box")
-        ax.axis('off')
+        plt.axis('on')
         plt.legend()
         plt.show(block=True)
 
-    def __getitem__(self, idx) -> Tuple[t.Image, Dict]:
+    def __getitem__(self, image_id) -> Tuple[t.Image, Dict]:
         """
         Get an item from the dataset
 
-        :param idx: The index of the item to retrieve
+        :param image_id: The index of the item to retrieve, i.e. image ID
 
         :return: A tuple containing the image tensor and a dictionary containing label and other stuff.
 
 
         See https://docs.pytorch.org/tutorials/intermediate/torchvision_tutorial.html
         """
-        images: List[Dict] = self.labels['images']
-        img_annotations: List[Dict] = [img_annotation for img_annotation in self.labels['annotations'] if
-                                       img_annotation['image_id'] == idx]
-        img_path = os.path.join(self.img_dir, images[idx]['file_name'])
-        image: torch.Tensor = read_image(str(img_path), ImageReadMode.RGB).float() / 255.0
+
+        img_annotations: List[Dict] = [img_annotation for img_annotation in self.annotations if
+                                       img_annotation['image_id'] == image_id]
+
+        img_path = os.path.join(self.img_dir, self.images[image_id]['file_name'])
+        image: torch.Tensor = read_image(str(img_path), ImageReadMode.RGB)#.float() / 255.0
         target: Dict = {}
 
         category_ids: List[int] = [img_annotation['category_id'] for img_annotation in img_annotations]
         bboxes: List[List[int]] = [ann['bbox'] for ann in img_annotations]
 
-        image_id: int = images[idx]['id']  # must be unique for each image, used during evaluation
         for img_annotation in img_annotations:
             if img_annotation['category_id'] == 4:  # arrow
                 keypoints: List[float] = img_annotation['keypoints']
                 if image_id == 168:
-                    self.plot_image_with_keypoints_and_bbox(image_tensor=image, keypoints=[[keypoints[3], keypoints[4]], [keypoints[0], keypoints[1]]], bbox=img_annotation['bbox'])
+                    self.plot_image_with_keypoints_and_bbox(image_tensor=image, keypoints=keypoints, bbox=img_annotation['bbox'])
 
                 bbox_head: List[int] = self.generate_synthetic_bbox(image=image, center_x=int(keypoints[3]),
-                                                                    center_y=int(keypoints[4]), image_id=image_id)
+                                                                    center_y=int(keypoints[4]))
                 category_ids.append(10)
                 bboxes.append(bbox_head)
                 bbox_tail: List[int] = self.generate_synthetic_bbox(image=image, center_x=int(keypoints[0]),
-                                                                    center_y=int(keypoints[1]), image_id=image_id)
+                                                                    center_y=int(keypoints[1]))
                 category_ids.append(11)
                 bboxes.append(bbox_tail)
 
@@ -147,9 +150,48 @@ class ObjectDetectionDataset(Dataset):
         target['image_id']: torch.Tensor = torch.tensor([image_id])
         target['area']: torch.Tensor = area
         target['iscrowd']: torch.Tensor = iscrowd
+
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             target = self.target_transform(target)
 
         return t.Image(image), target
+
+
+if __name__ == '__main__':
+
+    import cv2
+    import json
+    import matplotlib.pyplot as plt
+
+    BASE_DIR = "/home/nricciardi/Repositories/diagram"
+
+    dataset = ObjectDetectionDataset(f"{BASE_DIR}/dataset/extractor/labels.json", f"{BASE_DIR}/dataset/extractor/flow_graph_diagrams")
+
+    # for _ in dataset:
+    #     pass
+
+    with open(f"{BASE_DIR}/dataset/extractor/labels.json", "r") as info:
+        info = json.load(info)
+        for annotation in info["annotations"]:
+            if annotation["category"] == "arrow":
+                print(annotation)
+                image_file_name = info["images"][annotation["image_id"]]["file_name"]  # not always true... check id instead
+
+                image = read_image(f'{BASE_DIR}/dataset/extractor/flow_graph_diagrams/{image_file_name}', ImageReadMode.RGB)
+
+                head_x = annotation["keypoints"][3]
+                head_y = annotation["keypoints"][4]
+
+                tail_x = annotation["keypoints"][0]
+                tail_y = annotation["keypoints"][1]
+
+                plt.imshow(image.permute(1, 2, 0).cpu().numpy())
+                plt.scatter([tail_x], [tail_y], c='red', s=40)
+                plt.scatter([head_x], [head_y], c='blue', s=40)
+                plt.show()
+
+                outcome = dataset.generate_synthetic_bbox(image, tail_x, tail_y)
+
+                break
